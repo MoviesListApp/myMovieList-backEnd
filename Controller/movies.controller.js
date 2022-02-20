2661226fe28987ab1d343ae118c2a99e46ebdc01
rawankh98 <rawanahmed115@gmail.com>
2022-02-20 22:58:29 +0200

    new edition

---
+++ Controller/movies.controller.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const pg = require('pg');
+require('dotenv').config();
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cors());
+const axios = require('axios');
+const { request, response } = require('express');
+const my_api = process.env.APIKEY;
+
+// const { request, response } = require("express");
+const client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+})
+
+
+function Movies(title, poster_path, overview, release_dates, comment){
+    this.title = title;
+    this.poster_path = poster_path;
+    this.overview = overview;
+    this.release_dates = release_dates;
+    this.comment = comment;
+
+}
+
+
+ const getAllMovies = (request, response)=>{
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${my_api}&language=en-US&page=1`).then(data => {
+        let mymovie = data.data.results.map((value) =>{
+            let newMovie = new Movies(value.title, value.poster_path, value.overview, value.overview, value.release_dates, value.comment);
+            return newMovie;
+        });
+        // console.log(mymovie);
+        response.send(mymovie);
+    }).catch((err)=>{
+        console.log(err);
+        response.send(err);
+    })
+}
+
+const AddMovie = (request, response)=>{
+    let req = request.body;
+    console.log('====================================');
+    console.log(req);
+    console.log('====================================');
+    let newMovie = [req.title, req.poster_path, req.overview, req.release_dates, req.comment];
+    let insertSql = `INSERT INTO mymovies(title,  poster_path, overview, release_dates, comment) VALUES($1, $2, $3, $4, $5);`;
+    client.query(insertSql, newMovie).then(data => {
+        console.log(data);
+        // response.json(data.rows);
+response.send("Hello")
+    }).catch((error)=>{
+        console.log('====================================');
+        console.log(error);
+        console.log('====================================');
+    });
+    
+}
+
+const deleteMovie = ((request, response)=>{
+    const id = request.params.id;
+    console.log(id);
+    let delSql = `DELETE FROM myMovies WHERE id = ${id};`;
+    client.query(delSql).then(results => {
+        console.log(results);
+        return response.status(200).send(results.rows);
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+const updateMovie = ((request, response)=>{
+    const id = request.params.id;
+    let comm = [request.body.comment];
+    console.log(id);
+    console.log(comm);
+    let upSql = `UPDATE myMovies SET comment = $1 WHERE id = ${id} RETURNING *;`;
+    client.query(upSql, comm)
+        .then(results => {
+            console.log(results.rows);
+            response.status(200).send('your data has been updated successfully')
+        })
+        .catch(console.error)
+})
+
+const getMovie = ((request, response)=>{
+    const id = request.params.id;
+    let sqlGetMyMovie = `SELECT * FROM myMovies WHERE id = ${id}`;
+    client.query(sqlGetMyMovie).then(results => {
+        response.send(results.rows);
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+module.exports = {getAllMovies, deleteMovie, AddMovie, updateMovie, getMovie, client};
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # fullCrudApp
-///full
